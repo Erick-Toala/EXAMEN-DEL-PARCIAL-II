498466d96916a14ffc5effb325d8165adc5158b4
Erick-Toala <94879384+Erick-Toala@users.noreply.github.com>
2022-01-21 18:11:06 -0500

    Add files via upload

---
+++ Patrón_Singleton/Patrón_Singleton_Examen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Patrón_Singleton_Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //obtenemos la instancia, se crea por primera vez 
+            Singleton persona = Singleton.ObtenerInstancia();
+
+            //Hacemos algo con la instancia
+            persona.AgregarDatos("Erick",20);
+            persona.proceso();
+            Console.WriteLine(persona);
+            Console.WriteLine("-------------------------------------------\n");
+            //obtenemos la instancia 
+            Singleton persona2 = Singleton.ObtenerInstancia();
+
+            //comprobamos que es la misma instancia
+            //si lo es, tendrá el mismo estado
+            Console.WriteLine(persona2);
+        }
+    }
+}
---
+++ Patrón_Singleton/Patrón_Singleton_Examen/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patrón_Singleton_Examen
+{
+    class Singleton
+    {
+        //Aqui guardamos la unica instancia que va a existir 
+        private static Singleton instancia;
+
+        //Datos propios de la clase que pertenecen a una persona
+        private string nombre;
+        private int edad;
+
+        //creamos un constructor privado
+        private Singleton()
+        {
+            nombre = "Sin asignar";
+            edad = 0;
+        }
+        public static Singleton ObtenerInstancia()
+        {
+            //verificamos si no existe la instancia 
+            if (instancia == null)
+            {
+                //si no existe, instanciamos
+                instancia = new Singleton();
+                Console.WriteLine("---- Instancia creada por primera vez -----");
+            }
+            //regresamos la instancia
+            return instancia;
+        }
+        //aquí metodos propios de la clase 
+        public override string ToString()
+        {
+            return string.Format("La persona "+nombre+", tiene una edad de "+edad+" años");
+        }
+        public void AgregarDatos(string nombre1, int edad1)
+        {
+            nombre = nombre1;
+            edad = edad1;
+        }
+        //esto representa cualquier otro objeto 
+        public void proceso()
+        {
+            Console.WriteLine(nombre+" está trabajando");   
+        }
+    }
+}
